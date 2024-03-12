d7974fbcb7c3bf5e3a97cbc21a1d8a86ebea74d9
Marvin <okongomarvin971@gmail.com>
2024-03-12 09:58:08 +0300

    iNCORRECT ANSWER

--- lib/final_bal.py
+++ lib/final_bal.py
@@ -1,14 +1,12 @@
 def solution(A, D):
-    # Initialize variables
     balance = 0
     card_payments = 0
     card_payment_cost = 0
     monthly_fee = 5
-    current_month = int(D[0].split('-')[1])  # Extract month from the first date
+    current_month = int(D[0].split('-')[1])
     month_count = {i: 0 for i in range(1, 13)}
     fee_paid = {i: False for i in range(1, 13)}
 
-    # Process transactions
     for i in range(len(A)):
         balance += A[i]
 
@@ -19,7 +17,6 @@ def solution(A, D):
         month = int(D[i].split('-')[1])
         month_count[month] += 1
 
-        # Check if fee was paid for this month
         if not fee_paid[month]:
             if month_count[month] >= 3 and card_payment_cost >= 100:
                 fee_paid[month] = True
